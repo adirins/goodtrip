65d7eca062c7efc55fcb00244fefd81858b5a09c
adirins <andris.dirins@tet.lv>
2025-04-20 22:40:27 +0300

    feat: clean up, tailwind, font awesome

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fas, far);
+
+export const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="h-full mx-auto relative">
+    <div class="w-full max-w-[1024px] xl:max-w-[1536] xl:min-w-[1025px] bg-gray-200 rounded shadow-xl grid gap-2.5 p-5 m-5">
+      <h1 class="text-xl font-medium text-gray-700">Weather app</h1>
+
+      <div class="flex flex-wrap gap-10 w-full">
+        Select city
+
+        Select units
+      </div>
+    </div>
+
+  </div>
 </template>
